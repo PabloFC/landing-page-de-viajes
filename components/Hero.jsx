3c72b2794529c9aac291a779172c002aa1c6202f
PabloFC <pablofc_68@hotmail.com>
2025-05-04 21:47:24 +0200

    Hero component added

---
+++ components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <h1>Descuentos hasta el 30%</h1>
+            <p>Tu próxima aventura te espera</p>
+            <a href="#" className="btn btn-primary">
+              Explorar destinos
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import Head from "next/head";
 
 export default function Home() {
@@ -11,22 +12,7 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      {/*  Hero  */}
-
-      <section className="hero-section">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <h1>Welcome to Our Website</h1>
-              <p>This is a simple hero section created using Bootstrap.</p>
-              <a href="#" className="btn btn-primary">
-                Get Started
-              </a>
-            </div>
-          </div>
-        </div>
-      </section>
-      {/*  Hero */}
+      <Hero />
     </>
   );
 }
