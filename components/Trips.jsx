29922a0f73a2a9b01a7e98d7bc22f869d707153a
PabloFC <pablofc_68@hotmail.com>
2025-05-22 19:32:58 +0200

    code reduced in trips.jsx component

--- components/Trips.jsx
+++ components/Trips.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Trips = () => {
+  const [destinos, setDestinos] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/trips.json")
+      .then((res) => res.json())
+      .then((data) => setDestinos(data));
+  }, []);
+
   return (
     <>
       <div className="d-flex flex-column justify-content-center align-items-center py-5">
@@ -10,48 +18,18 @@ const Trips = () => {
 
       <div className="container">
         <div className="gallery">
-          <figure className="gallery__item gallery__item--1">
-            <img
-              src="/images/sydney.jpg"
-              alt="Gallery image 1"
-              className="gallery__img"
-            />
-          </figure>
-          <figure className="gallery__item gallery__item--2">
-            <img
-              src="/images/rome.jpg"
-              alt="Gallery image 2"
-              className="gallery__img"
-            />
-          </figure>
-          <figure className="gallery__item gallery__item--3">
-            <img
-              src="/images/nyc.jpg"
-              alt="Gallery image 3"
-              className="gallery__img"
-            />
-          </figure>
-          <figure className="gallery__item gallery__item--4">
-            <img
-              src="/images/paris.jpg"
-              alt="Gallery image 4"
-              className="gallery__img"
-            />
-          </figure>
-          <figure className="gallery__item gallery__item--5">
-            <img
-              src="/images/rio.jpg"
-              alt="Gallery image 5"
-              className="gallery__img"
-            />
-          </figure>
-          <figure className="gallery__item gallery__item--6">
-            <img
-              src="/images/tokio.jpg"
-              alt="Gallery image 6"
-              className="gallery__img"
-            />
-          </figure>
+          {destinos.map((destino, idx) => (
+            <figure
+              key={destino.id}
+              className={`gallery__item gallery__item--${idx + 1}`}
+            >
+              <img
+                src={destino.image}
+                alt={destino.title}
+                className="gallery__img"
+              />
+            </figure>
+          ))}
         </div>
       </div>
     </>
