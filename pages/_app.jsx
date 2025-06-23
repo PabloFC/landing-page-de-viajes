e58b257ddea32a0818b9c74296d89febe0d48eab
PabloFC <pablofc_68@hotmail.com>
2025-06-24 00:40:31 +0200

    lato font imported

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,17 @@
+import { Lato } from "next/font/google";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
